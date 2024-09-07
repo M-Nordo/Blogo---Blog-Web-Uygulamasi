4a6b2c05ed31baf4a1b08ee59aabc896991dec3e
Mehmet Yayla <69403373+M-Nordo@users.noreply.github.com>
2024-09-07 23:11:10 +0300

    Update db.js

--- blogo-blog/lib/config/db.js
+++ blogo-blog/lib/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const ConnectDB = async () => {
-    await mongoose.connect('mongodb+srv://nordo:Nordo12345678@cluster0.jikiu.mongodb.net/blogo')
+    await mongoose.connect('mongodb+srv://username:password@cluster0.jikiu.mongodb.net/blogo')
     console.log("veritabanı bağlandı");
 }
